dafa5abdfda7bfd77af3db7eb8b1af9845185f35
marvan05 <11946237@g.nwu.ac.za>
2021-10-25 20:30:22 +0200

    2021-10-25
    Registration and Login

---
+++ PHOTOSHARE/PHOTOSHARE/Areas/Identity/Data/PHOTOSHAREDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PHOTOSHARE.Areas.Identity.Data;
+
+namespace PHOTOSHARE.Data
+{
+    public class PHOTOSHAREDBContext : IdentityDbContext<PHOTOSHAREUser>
+    {
+        public PHOTOSHAREDBContext(DbContextOptions<PHOTOSHAREDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}

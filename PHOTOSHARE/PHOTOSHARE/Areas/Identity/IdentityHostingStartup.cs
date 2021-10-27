d95a844fee9969d9ff436b26aa3130a4d9819daa
marvan05 <11946237@g.nwu.ac.za>
2021-10-27 18:21:27 +0200

    2021-10-27
    Add New Scaffold Item Identity

---
+++ PHOTOSHARE/PHOTOSHARE/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PHOTOSHARE.Areas.Identity.Data;
+using PHOTOSHARE.Data;
+
+[assembly: HostingStartup(typeof(PHOTOSHARE.Areas.Identity.IdentityHostingStartup))]
+namespace PHOTOSHARE.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PHOTOSHAREDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PHOTOSHAREDBContextConnection")));
+
+                services.AddDefaultIdentity<PHOTOSHAREUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PHOTOSHAREDBContext>();
+            });
+        }
+    }
+}

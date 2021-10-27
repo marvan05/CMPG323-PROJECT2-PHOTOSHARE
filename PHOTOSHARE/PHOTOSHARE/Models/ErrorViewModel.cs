e4979dd17b91b12d403252b9b727671cd6922c58
marvan05 <11946237@g.nwu.ac.za>
2021-10-27 08:36:47 +0200

    2021-10-27

---
+++ PHOTOSHARE/PHOTOSHARE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PHOTOSHARE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

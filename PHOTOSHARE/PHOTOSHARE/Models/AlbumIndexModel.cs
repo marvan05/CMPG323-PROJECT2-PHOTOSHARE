848106ff4e3046cc5c5afef8493f41d91df1ecac
PierreG71 <pierreg@allems.co.za>
2021-11-11 12:20:12 +0200

    added album.jpg and modify controllers

---
+++ PHOTOSHARE/PHOTOSHARE/Models/AlbumIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PHOTOSHARE.Models
+{
+    public class AlbumIndexModel
+    {
+        public IEnumerable<AlbumImage> Albums { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}

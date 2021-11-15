0bb17bfc96cd9a4444e1ee366d9b7cf11f798461
PierreG71 <pierreg@allems.co.za>
2021-11-15 09:46:31 +0200

    Album Code

---
+++ PHOTOSHARE/PHOTOSHARE/Models/AlbumViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PHOTOSHARE.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PHOTOSHARE.Models
+{
+    public class AlbumViewModel 
+    {
+
+        public List<Album> Albums { get; set; }
+
+
+        public AlbumViewModel(PHOTOSHAREDBContext context)
+        {
+            Albums = context.Albums.ToList();
+        }
+    }
+}

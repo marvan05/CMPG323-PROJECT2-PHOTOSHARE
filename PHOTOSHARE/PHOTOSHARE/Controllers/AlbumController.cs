293457f4de9c4f52a218bdbc3ed9f7e416559705
marvan05 <11946237@g.nwu.ac.za>
2021-11-18 07:03:22 +0200

    2021-11-18
    CRUD

--- PHOTOSHARE/PHOTOSHARE/Controllers/AlbumController.cs
+++ PHOTOSHARE/PHOTOSHARE/Controllers/AlbumController.cs
@@ -4,6 +4,7 @@ using PHOTOSHARE.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace PHOTOSHARE.Controllers
@@ -21,5 +22,75 @@ namespace PHOTOSHARE.Controllers
         {
             return View(new AlbumViewModel(_context));
         }
+
+        public PartialViewResult CreateAlbum()
+        {
+            var albumViewModel = new AlbumViewModel();
+            return PartialView("_AddEditAlbum", albumViewModel);
+        }
+
+        public PartialViewResult EditAlbum(int albumId)
+        {
+            var albumViewModel = AlbumViewModel.GetAlbum(_context, albumId);
+            return PartialView("_AddEditAlbum", albumViewModel);
+        }
+
+        [HttpPost]
+        public HttpStatusCode EditAlbum(string title, string image, int id)
+        {
+            var album = _context.Albums.Where(a => a.Id == id).SingleOrDefault();
+
+
+            try
+            {
+                album.Title = title;
+                album.Image = String.IsNullOrEmpty(image) ? album.Image : image;
+                _context.SaveChanges();
+                return HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+        }
+
+        [HttpPost]
+        public HttpStatusCode CreateAlbum(string title, string image)
+        {
+            var album = new Album
+            {
+                Title = title,
+                Owner = User.Identity.Name.ToString(),
+                Created = DateTime.Now,
+                Image = image,
+            };
+            try
+            {
+                _context.Albums.Add(album);
+                _context.SaveChanges();
+                return HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+        }
+
+        [HttpPost]
+        public HttpStatusCode DeleteAlbum(int Id)
+        {
+            var album = _context.Albums.Where(a => a.Id == Id).FirstOrDefault();
+            try
+            {
+                _context.Albums.Attach(album);
+                _context.Albums.Remove(album);
+                return HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+        }
     }
 }
+
--- PHOTOSHARE/PHOTOSHARE/Models/AlbumViewModel.cs
+++ PHOTOSHARE/PHOTOSHARE/Models/AlbumViewModel.cs
@@ -11,10 +11,22 @@ namespace PHOTOSHARE.Models
 
         public List<Album> Albums { get; set; }
 
+        public Album Album { get; set; }
+
+        public AlbumViewModel() { }
 
         public AlbumViewModel(PHOTOSHAREDBContext context)
         {
             Albums = context.Albums.ToList();
+        }
+
+        public static AlbumViewModel GetAlbum(PHOTOSHAREDBContext context, int albumId)
+        {
+            return new AlbumViewModel
+            {
+                Album = context.Albums.Where(a => a.Id == albumId).FirstOrDefault(),
+            };
+
         }
     }
 }

f65fb17b54780bb50b46b1f023528f38f7d4f32f
marvan05 <11946237@g.nwu.ac.za>
2021-11-19 19:14:53 +0200

    2021-11-19

--- PHOTOSHARE/PHOTOSHARE/obj/Release/netcoreapp3.1/Razor/Views/Home/Privacy.cshtml.g.cs
+++ PHOTOSHARE/PHOTOSHARE/obj/Release/netcoreapp3.1/Razor/Views/Home/Privacy.cshtml.g.cs
@@ -27,7 +27,7 @@ using PHOTOSHARE.Models;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d5403c4374377ef96d5bb7fc4426396a7bff53ca", @"/Views/Home/Privacy.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"88678c08d8cece9ae27746b2eef2d8d82ffcec5d", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"bd1f88fa0843f5c74b36eca495447da66d4f638f", @"/Views/_ViewImports.cshtml")]
     public class Views_Home_Privacy : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
--- PHOTOSHARE/PHOTOSHARE/obj/Release/netcoreapp3.1/Razor/Views/Shared/Error.cshtml.g.cs
+++ PHOTOSHARE/PHOTOSHARE/obj/Release/netcoreapp3.1/Razor/Views/Shared/Error.cshtml.g.cs
@@ -27,7 +27,7 @@ using PHOTOSHARE.Models;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d6a5625cc8fb4476f348b0fe9041c550465d8bf9", @"/Views/Shared/Error.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"88678c08d8cece9ae27746b2eef2d8d82ffcec5d", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"bd1f88fa0843f5c74b36eca495447da66d4f638f", @"/Views/_ViewImports.cshtml")]
     public class Views_Shared_Error : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<ErrorViewModel>
     {
         #pragma warning disable 1998
